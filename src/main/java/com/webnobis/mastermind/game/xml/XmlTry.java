7a2116b5f41743949b61f5e4f3d2481d14a67a44
steffen <github@webnobis.com>
2016-11-10 21:43:46 +0100

    xml to set added

--- src/main/java/com/webnobis/mastermind/game/xml/XmlTry.java
+++ src/main/java/com/webnobis/mastermind/game/xml/XmlTry.java
@@ -1,26 +1,35 @@
 package com.webnobis.mastermind.game.xml;
 
 import java.io.CharArrayWriter;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.JAXB;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "try")
 public class XmlTry<E> {
 
+	@XmlAttribute
+	private final String id;
+
 	@XmlElement
 	private final List<E> test;
 
-	public XmlTry(List<E> test) {
+	public XmlTry(String id, List<E> test) {
+		this.id = id;
 		this.test = test;
 	}
 
 	@OnlyForXmlTransformation
 	XmlTry() {
-		this(Collections.emptyList());
+		this(null, new ArrayList<>());
+	}
+
+	public String getId() {
+		return id;
 	}
 
 	public List<E> getTest() {
