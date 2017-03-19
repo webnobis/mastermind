2eb4061c9e2f0ffd630d6e65734d6be2b0cfcfd7
steffen <github@webnobis.com>
2017-03-19 21:48:23 +0100

    handler for swagger documentation bound

--- src/main/java/com/webnobis/mastermind/service/MastermindService.java
+++ src/main/java/com/webnobis/mastermind/service/MastermindService.java
@@ -1,6 +1,7 @@
 package com.webnobis.mastermind.service;
 
 import com.webnobis.mastermind.service.handler.GameHandler;
+import com.webnobis.mastermind.service.handler.SwaggerDocumentationHandler;
 
 import ratpack.server.RatpackServer;
 
@@ -9,10 +10,13 @@ public interface MastermindService {
 	static RatpackServer create() throws Exception {
 		return RatpackServer.of(spec -> spec
 				.registryOf(registry -> registry
-						.add(new GameHandler()))
+						.add(new GameHandler())
+						.add(new SwaggerDocumentationHandler()))
 				.handlers(chain -> chain
 						.post("game", GameHandler.class)
-						.get("hello", ctx -> ctx.render("Hello from MastermindService"))));
+						.get("hello", ctx -> ctx.render("Hello from MastermindService"))
+						.get("documentation", SwaggerDocumentationHandler.class)
+						.get("", ctx -> ctx.redirect("documentation"))));
 	};
 
 	public static void main(String[] args) throws Exception {
---
+++ src/main/java/com/webnobis/mastermind/service/handler/SwaggerDocumentationHandler.java
@@ -0,0 +1,14 @@
+package com.webnobis.mastermind.service.handler;
+
+import ratpack.handling.Context;
+import ratpack.handling.Handler;
+
+public class SwaggerDocumentationHandler implements Handler {
+
+	@Override
+	public void handle(Context ctx) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
