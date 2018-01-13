5adff3a5073e81c39edc3002642e4da248ddb4ca
steffen <github@webnobis.com>
2018-01-13 14:49:27 +0100

    assessment service and game updater empty created

---
+++ src/main/java/com/webnobis/mastermind/service/AssessmentService.java
@@ -0,0 +1,15 @@
+package com.webnobis.mastermind.service;
+
+import com.webnobis.mastermind.model.Try;
+import com.webnobis.mastermind.model.TryWithAssessment;
+
+public abstract class AssessmentService {
+
+	private AssessmentService() {
+	}
+
+	public static TryWithAssessment assess(Try theTry) {
+		return null;
+	}
+	
+}
---
+++ src/main/java/com/webnobis/mastermind/service/GameUpdateService.java
@@ -0,0 +1,15 @@
+package com.webnobis.mastermind.service;
+
+import com.webnobis.mastermind.model.GameWithSolution;
+import com.webnobis.mastermind.model.TryWithAssessment;
+
+public abstract class GameUpdateService {
+
+	private GameUpdateService() {
+	}
+
+	public static GameWithSolution update(GameWithSolution gameWithSolution, TryWithAssessment tryWithAssessment) {
+		return null;
+	}
+
+}
