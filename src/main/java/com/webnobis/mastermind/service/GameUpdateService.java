6834ca283c81b3be494c0ede17f3481811294514
steffen <github@webnobis.com>
2018-01-14 20:22:56 +0100

    update service finished

--- src/main/java/com/webnobis/mastermind/service/GameUpdateService.java
+++ src/main/java/com/webnobis/mastermind/service/GameUpdateService.java
@@ -1,7 +1,14 @@
 package com.webnobis.mastermind.service;
 
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.webnobis.mastermind.model.Game;
 import com.webnobis.mastermind.model.GameWithSolution;
 import com.webnobis.mastermind.model.TryWithAssessment;
+import com.webnobis.mastermind.model.xml.XmlGame;
+import com.webnobis.mastermind.model.xml.XmlGameWithSolution;
 
 public abstract class GameUpdateService {
 
@@ -9,7 +16,13 @@ public abstract class GameUpdateService {
 	}
 
 	public static GameWithSolution update(GameWithSolution gameWithSolution, TryWithAssessment tryWithAssessment) {
-		return null;
+		Objects.requireNonNull(gameWithSolution, "gameWithSolution is null");
+		Objects.requireNonNull(tryWithAssessment, "tryWithAssessment is null");
+		return new XmlGameWithSolution(gameWithSolution.getId(), update(gameWithSolution.getGame(), tryWithAssessment), gameWithSolution.getSolution());
+	}
+
+	private static Game update(Game game, TryWithAssessment tryWithAssessment) {
+		return new XmlGame(game.getConfig(), Stream.concat(game.getTries().stream(), Stream.of(tryWithAssessment)).collect(Collectors.toList()));
 	}
 
 }
