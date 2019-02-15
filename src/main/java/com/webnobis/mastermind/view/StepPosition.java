f2bdf43e65f7a3005ad9c01d35202607c735b89e
steffen <github@webnobis.com>
2019-02-15 22:44:23 +0100

    3d but not rendered

--- src/main/java/com/webnobis/mastermind/view/StepPosition.java
+++ src/main/java/com/webnobis/mastermind/view/StepPosition.java
@@ -5,10 +5,16 @@ import com.webnobis.mastermind.model.Status;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
+import javafx.scene.effect.BlendMode;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Material;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Circle;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.CullFace;
+import javafx.scene.shape.DrawMode;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape3D;
+import javafx.scene.shape.Sphere;
 
 public class StepPosition<T> extends Label {
 
@@ -16,7 +22,13 @@ public class StepPosition<T> extends Label {
 		super(type.toString());
 		super.setAlignment(Pos.CENTER);
 		if (Color.class.isAssignableFrom(type.getClass())) {
-			super.setGraphic(new Circle(10, Color.class.cast(type)));
+			PhongMaterial material = new PhongMaterial();
+			material.setDiffuseColor(Color.class.cast(type));
+	        material.setSpecularColor(Color.BLACK);
+	        material.setSpecularPower(100);
+			Shape3D shape = new Sphere(20.0);
+			shape.setMaterial(material);
+			super.setGraphic(shape);
 			super.setTooltip(new Tooltip(super.getText()));
 			super.setText("");
 		} else if (Status.class.isAssignableFrom(type.getClass())) {
