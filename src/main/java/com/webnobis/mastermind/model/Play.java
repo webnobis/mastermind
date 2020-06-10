8569d3b2ea5f835f1736a079421f50510eb36ef0
Steffen Nobis <steffen@webnobis.com>
2020-06-10 17:38:15 +0200

    optimized play added

--- src/main/java/com/webnobis/mastermind/model/Play.java
+++ src/main/java/com/webnobis/mastermind/model/Play.java
@@ -1,15 +1,20 @@
 package com.webnobis.mastermind.model;
 
-import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Play<T extends Serializable> implements Serializable {
+public class Play<T> {
 
-	private static final long serialVersionUID = 1L;
+	private final String id;
+
+	private final Class<T> type;
+
+	private final int cols;
 
 	private final int rows;
 
@@ -17,29 +22,43 @@ public class Play<T extends Serializable> implements Serializable {
 
 	private final Source<T> source;
 
-	Play(int rows, List<Result<T>> results, Source<T> source) {
+	Play(String id, Class<T> type, int cols, int rows, List<Result<T>> results, Source<T> source) {
+		this.id = Objects.requireNonNull(id, "id is null");
+		this.type = Objects.requireNonNull(type, "type is null");
+		this.cols = cols;
 		this.rows = rows;
-		this.results = results;
+		this.results = Objects.requireNonNull(results, "results is null");
 		this.source = source;
 	}
 
-	public static <T extends Serializable> Play<T> of(Source<T> source) {
-		return of(-1, source);
+	public static <T> Play<T> of(Class<T> type, int cols) {
+		return of(type, cols, -1);
 	}
 
-	public static <T extends Serializable> Play<T> of(int rows, Source<T> source) {
-		return new Play<>(rows, Collections.emptyList(), Objects.requireNonNull(source, "source is null"));
+	public static <T> Play<T> of(Class<T> type, int cols, int rows) {
+		return new Play<>(UUID.randomUUID().toString(), type, cols, rows, Collections.emptyList(), null);
 	}
 
-	public Play<T> ofAddedResult(Result<T> result) {
-		return new Play<>(rows,
-				Stream.concat(results.stream(), Stream.of(Objects.requireNonNull(result, "result is null")))
-						.collect(Collectors.toList()),
-				source);
+	public Play<T> withAddedResult(Result<T> result) {
+		return Optional.ofNullable(result).map(Stream::of)
+				.map(resultStream -> Stream.concat(results.stream(), resultStream).collect(Collectors.toList()))
+				.map(resultList -> new Play<>(id, type, cols, rows, resultList, source)).orElse(this);
+	}
+
+	public Play<T> withSource(Source<T> source) {
+		return new Play<>(id, type, cols, rows, results, source);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public Class<T> getType() {
+		return type;
 	}
 
 	public int getCols() {
-		return Objects.requireNonNull(source.getSources(), "sources of source is null").size();
+		return cols;
 	}
 
 	public int getRows() {
@@ -65,12 +84,7 @@ public class Play<T extends Serializable> implements Serializable {
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((results == null) ? 0 : results.hashCode());
-		result = prime * result + rows;
-		result = prime * result + ((source == null) ? 0 : source.hashCode());
-		return result;
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -78,37 +92,15 @@ public class Play<T extends Serializable> implements Serializable {
 		if (this == obj) {
 			return true;
 		}
-		if (obj == null) {
+		if (obj == null || getClass() != obj.getClass()) {
 			return false;
 		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Play other = (Play) obj;
-		if (results == null) {
-			if (other.results != null) {
-				return false;
-			}
-		} else if (!results.equals(other.results)) {
-			return false;
-		}
-		if (rows != other.rows) {
-			return false;
-		}
-		if (source == null) {
-			if (other.source != null) {
-				return false;
-			}
-		} else if (!source.equals(other.source)) {
-			return false;
-		}
-		return true;
+		return Objects.equals(id, Play.class.cast(obj).id);
 	}
 
 	@Override
 	public String toString() {
-		return "Play [rows=" + rows + ", source=" + source + ", results=" + results.size() + ", finish=" + isFinish()
-				+ ", solved=" + isSolved() + "]";
+		return "Play [id=" + id + "]";
 	}
 
 }
--- src/test/java/com/webnobis/mastermind/model/PlayTest.java
+++ src/test/java/com/webnobis/mastermind/model/PlayTest.java
@@ -1,10 +1,17 @@
 package com.webnobis.mastermind.model;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,7 +20,15 @@ import org.junit.jupiter.api.Test;
 
 class PlayTest {
 
-	private static final int ROWS = 3;
+	private static final String ID = "the id";
+
+	private static final Class<Color> TYPE = Color.class;
+
+	private static final int COLS = 3;
+
+	private static final int ROWS = 2;
+
+	private static final Result<Color> TRY1 = Result.of(Source.of(Color.RED), ResultType.PRESENT);
 
 	private static final Source<Color> SOURCE = Source.of(Color.BLUE, Color.RED, null);
 
@@ -21,42 +36,93 @@ class PlayTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		play = Play.of(ROWS, SOURCE);
+		play = new Play<>(ID, TYPE, COLS, ROWS, Collections.singletonList(TRY1), SOURCE);
+	}
+
+	@Test
+	void testOf() {
+		Play<Boolean> newPlay = Play.of(Boolean.class, COLS);
+
+		assertNotNull(newPlay.getId());
+		assertEquals(-1, newPlay.getRows());
+		assertNotNull(newPlay.getResults());
+		assertTrue(newPlay.getResults().isEmpty());
+		assertNull(newPlay.getSource());
+	}
+
+	@Test
+	void testOfNull() {
+		assertThrows(NullPointerException.class,
+				() -> new Play<Boolean>(null, Boolean.class, 1, 1, Collections.emptyList(), Source.of()));
+		assertThrows(NullPointerException.class,
+				() -> new Play<Byte>("id1", null, 1, 1, Collections.emptyList(), Source.of()));
+		assertThrows(NullPointerException.class, () -> new Play<Byte>("id1", Byte.class, 1, 1, null, Source.of()));
+		assertDoesNotThrow(() -> new Play<Short>("id1", Short.class, 1, 1, Collections.emptyList(), null));
+	}
+
+	@Test
+	void testWithAddedResult() {
+		Result<Color> result = Result.of(Source.of());
+		Play<Color> newPlay = play.withAddedResult(result);
+
+		assertEquals(play.getId(), newPlay.getId());
+		assertEquals(play.getType(), newPlay.getType());
+		assertSame(play.getCols(), newPlay.getCols());
+		assertSame(play.getRows(), newPlay.getRows());
+		assertEquals(play.getSource(), newPlay.getSource());
+
+		assertSame(2, newPlay.getResults().size());
+		assertTrue(newPlay.getResults().contains(result));
+	}
+
+	@Test
+	void testWithSource() {
+		assertNull(play.withSource(null).getSource());
+	}
+
+	@Test
+	void testGetId() {
+		assertEquals(ID, play.getId());
+	}
+
+	@Test
+	void testGetType() {
+		assertEquals(TYPE, play.getType());
 	}
 
 	@Test
 	void testGetCols() {
-		assertEquals(SOURCE.getSources().size(), play.getCols());
+		assertSame(COLS, play.getCols());
 	}
 
 	@Test
 	void testGetRows() {
-		assertEquals(ROWS, play.getRows());
-
-		assertTrue(Play.of(Source.of()).getRows() < 0);
+		assertSame(ROWS, play.getRows());
 	}
 
 	@Test
 	void testGetResults() {
-		assertTrue(play.getResults().isEmpty());
+		List<Result<Color>> results = play.getResults();
 
-		assertSame(1, play.ofAddedResult(Result.of(Source.of())).getResults().size());
+		assertNotNull(results);
+		assertSame(1, results.size());
+		assertEquals(TRY1, results.iterator().next());
 	}
 
 	@Test
 	void testGetSource() {
-		assertEquals(SOURCE, play.getSource());
+		assertEquals(SOURCE, play.withSource(SOURCE).getSource());
 	}
 
 	@Test
 	void testIsFinish() {
 		assertFalse(play.isFinish());
 
-		assertTrue(new Play<>(ROWS,
-				Stream.<Result<Color>>generate(() -> Result.of(Source.of())).limit(ROWS).collect(Collectors.toList()),
-				Source.of()).isFinish());
+		assertTrue(new Play<Object>("id 1", Object.class, COLS, ROWS,
+				Stream.generate(() -> Result.of(Source.of())).limit(ROWS).collect(Collectors.toList()), null)
+						.isFinish());
 
-		assertTrue(play.ofAddedResult(Result.of(Source.of((Color) null, (Color) null, (Color) null), ResultType.EXACT,
+		assertTrue(play.withAddedResult(Result.of(Source.of((Color) null, (Color) null, (Color) null), ResultType.EXACT,
 				ResultType.EXACT, ResultType.EXACT)).isFinish());
 	}
 
@@ -64,12 +130,26 @@ class PlayTest {
 	void testIsSolved() {
 		assertFalse(play.isSolved());
 
-		assertFalse(play.ofAddedResult(Result.of(SOURCE, ResultType.EXACT, ResultType.EXACT)).isSolved());
-		assertFalse(play.ofAddedResult(Result.of(SOURCE, ResultType.EXACT, ResultType.EXACT, ResultType.PRESENT))
+		assertFalse(play.withAddedResult(Result.of(SOURCE, ResultType.EXACT, ResultType.EXACT)).isSolved());
+		assertFalse(play.withAddedResult(Result.of(SOURCE, ResultType.EXACT, ResultType.EXACT, ResultType.PRESENT))
 				.isSolved());
+		assertTrue(play.withAddedResult(Result.of(SOURCE, ResultType.EXACT, ResultType.EXACT, ResultType.EXACT))
+				.isSolved());
+	}
 
-		assertTrue(
-				play.ofAddedResult(Result.of(SOURCE, ResultType.EXACT, ResultType.EXACT, ResultType.EXACT)).isSolved());
+	@Test
+	void testEquals() {
+		assertEquals(play, play);
+
+		assertEquals(play, play.withSource(null));
+		assertEquals(play, play.withSource(Source.of()));
+
+		assertEquals(play, play.withAddedResult(null));
+		assertEquals(play, play.withAddedResult(Result.of(Source.of())));
+
+		assertNotEquals(play, null);
+		assertNotEquals(play, new Object());
+		assertNotEquals(Play.of(Boolean.class, COLS), Play.of(Boolean.class, COLS));
 	}
 
 	enum Color {
