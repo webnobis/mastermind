ebaa8044cd7c7aa5fe4bf708d874a8b818d54bd6
Steffen Nobis <steffen@webnobis.com>
2020-06-11 19:43:22 +0200

    xml transformer added

--- src/main/java/com/webnobis/mastermind/service/XmlTransformer.java
+++ src/main/java/com/webnobis/mastermind/service/XmlTransformer.java
@@ -12,22 +12,45 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
-import com.webnobis.mastermind.model.Source;
-
+/**
+ * Transforms model classes from or to XML
+ * 
+ * @author steffen
+ *
+ */
 public interface XmlTransformer {
 
-	@SuppressWarnings("unchecked")
-	static <T> Source<T> toSource(String xml) {
+	/**
+	 * Transforms XML to model
+	 * 
+	 * @param <T>       model type
+	 * @param xml       XML
+	 * @param modelType model type class
+	 * @return model
+	 * 
+	 * @see JAXB#unmarshal(java.io.Reader, Class)
+	 */
+	static <T> T toModel(String xml, Class<T> modelType) {
 		try (CharArrayReader in = new CharArrayReader(Objects.requireNonNull(xml, "xml is null").toCharArray())) {
-			return JAXB.unmarshal(in, Source.class);
+			return JAXB.unmarshal(in, Objects.requireNonNull(modelType));
 		}
 	}
-	
+
+	/**
+	 * Transforms model to XML, encoded as UTF-8
+	 * 
+	 * @param model model
+	 * @return XML
+	 * 
+	 * @see Marshaller#JAXB_ENCODING
+	 * @see Marshaller#marshal(Object, java.io.Writer)
+	 */
 	static String toXml(Object model) {
 		try (CharArrayWriter out = new CharArrayWriter()) {
 			try {
 				Marshaller marshaller = JAXBContext.newInstance(model.getClass()).createMarshaller();
 				marshaller.setProperty(Marshaller.JAXB_ENCODING, StandardCharsets.UTF_8.name());
+				marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 				marshaller.marshal(model, out);
 			} catch (JAXBException e) {
 				throw new UncheckedIOException(new IOException(e));
--- src/test/java/com/webnobis/mastermind/service/XmlTransformerTest.java
+++ src/test/java/com/webnobis/mastermind/service/XmlTransformerTest.java
@@ -1,24 +1,37 @@
 package com.webnobis.mastermind.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.webnobis.mastermind.model.Play;
+import com.webnobis.mastermind.model.Result;
+import com.webnobis.mastermind.model.ResultType;
 import com.webnobis.mastermind.model.Source;
 
 class XmlTransformerTest {
-	
+
 	private static final Source<Boolean> SOURCE = Source.of(Boolean.TRUE, Boolean.FALSE);
+
+	private static final Play<Integer> PLAY = Play.of(Integer.class, 42).withSource(Source.of(17, 6, -19))
+			.withAddedResult(Result.of(Source.of(31), ResultType.PRESENT, ResultType.EXACT));
 
 	@Test
 	void testTransformSource() {
 		String xml = XmlTransformer.toXml(SOURCE);
-		
+
 		System.out.println(xml);
-		
-		assertEquals(SOURCE.getSources(), XmlTransformer.toSource(xml).getSources());
+
+		assertEquals(SOURCE.getSources(), XmlTransformer.toModel(xml, Source.class).getSources());
+	}
+
+	@Test
+	void testTransformPlay() {
+		String xml = XmlTransformer.toXml(PLAY);
+
+		System.out.println(xml);
+
+		assertEquals(PLAY, XmlTransformer.toModel(xml, Play.class));
 	}
 
 }
